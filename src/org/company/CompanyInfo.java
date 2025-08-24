43947fa8ac5ac5588d4095dba763d52d6e6f1c3c
harishraju <harishraju.03@gmail.com>
2025-08-24 16:56:01 +0530

    new java code of company details added

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,32 @@
+package org.company;
+
+public class CompanyInfo {
+	
+	//method creation
+	
+	private void CompanyName()
+	{
+		System.out.println("ABC Private Limited Company");
+	}
+	
+	private void CompanyID()
+	{
+		System.out.println("abc09876");
+	}
+	
+	private void CompanyAddress()
+	{
+		System.out.println("No 07 OMR ROad 1st Cross Thoraipakkam, Chennai 600028.");
+	}
+
+	public static void main(String[] args) {
+		//object creation
+		CompanyInfo c = new CompanyInfo();
+		//method calling
+		c.CompanyID();
+		c.CompanyName();
+		c.CompanyAddress();
+
+	}
+
+}
